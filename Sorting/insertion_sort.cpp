2662314f56d4128399e903102c576b4858fea6a9
Somnath Chakraborty <61010000+somu-bs@users.noreply.github.com>
2023-06-10 19:41:21 +0530

    Update insertion_sort.cpp

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -1,1 +1,12 @@
+TC - O(N*N) Worst/Average
+TC - O(N) Best
 
+void insertion_sort(int arr[], int n) {
+	for(int i = 0;i<n;i++) {
+		int j = i;
+		while(j > 0 && a[j-1] > a[j]) {
+			swap(a[j-1], a[j]);
+			j--;
+		}
+	}
+}
