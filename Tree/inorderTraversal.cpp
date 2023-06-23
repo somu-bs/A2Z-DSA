6e40d8e90184bca134911680af2567131e3bf823
Somnath Chakraborty <61010000+somu-bs@users.noreply.github.com>
2023-06-23 09:50:16 +0530

    Update inorderTraversal.cpp

--- Tree/inorderTraversal.cpp
+++ Tree/inorderTraversal.cpp
@@ -14,3 +14,29 @@ public:
         return inorder;
     }
 };
+
+
+// Itetative traversal using stack
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        stack<TreeNode*> sk;
+        TreeNode* node = root;
+        while(true) {
+            if(node != NULL) {
+                sk.push(node);
+                node = node->left;
+            }
+            else {
+                if(sk.empty()) break;
+                node = sk.top();
+                sk.pop();
+                inorder.push_back(node->val);
+                node = node->right;
+            }
+        }
+        return inorder;
+    }
+};
