3e8315b1753b616638eaf6d0b00b71b68d8a6faa
Somnath Chakraborty <61010000+somu-bs@users.noreply.github.com>
2023-06-26 20:55:41 +0530

    Update preorderTraversal.cpp

--- Tree/preorderTraversal.cpp
+++ Tree/preorderTraversal.cpp
@@ -14,3 +14,25 @@ public:
         return preorder;
     }
 };
+
+
+// Iterative traversal using stack
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        if(root == NULL) return preorder;
+        stack<TreeNode*> st;
+        st.push(root); // 3
+        while(!st.empty()) {
+            TreeNode* node = st.top(); // 1 2 3
+            st.pop();
+
+            if(node->right) st.push(node->right);
+            if(node->left) st.push(node->left); // 3
+            
+            preorder.push_back(node->val); // 1 2
+        }
+        return preorder;
+    }
+};
