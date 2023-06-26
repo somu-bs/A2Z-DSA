4277350704aa8fbd7a5ca1f7223592960f77462b
Somnath Chakraborty <61010000+somu-bs@users.noreply.github.com>
2023-06-26 21:40:03 +0530

    Update postorderTraversal.cpp

--- Tree/postorderTraversal.cpp
+++ Tree/postorderTraversal.cpp
@@ -14,3 +14,27 @@ public:
         return postorder;
     }
 };
+
+
+// Iterative traversal using 2 stack
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> postorder;
+        if(!root) return postorder;
+        stack<TreeNode*> st1, st2;
+        st1.push(root);
+        while(!st1.empty()) {
+            TreeNode* node = st1.top(); // 
+            st1.pop();
+            if(node->left) st1.push(node->left); // 3
+            if(node->right) st1.push(node->right);
+            st2.push(node); // 1 2 3
+        }
+        while(!st2.empty()) {
+            postorder.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return postorder;
+    }
+};
